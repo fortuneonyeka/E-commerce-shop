b54c084eee7f74c18571e2912b9f5eb49e0857ec
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-03-21 11:55:38 +0100

    add Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+import Home from './Components/Home'
+import About from './Components/About'
+import Contact from './Components/Contact'
+import Product from './Components/Product'
+import {BrowserRouter,Routes,Route,} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <BrowserRouter>
+      <Header />
+        <Routes>
+          <Route path="/" element={Home}/>
+          <Route path="about" element={About}/>
+          <Route path="contact" element={Contact}/>
+          <Route path="product" element={Product}/>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
