b678496dbec220add0f58988b1ffdb607067abb8
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-03-21 12:58:19 +0100

    change all a to Navlink and href to to..

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     <>
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -28,6 +28,7 @@ const Header = () => {
         </li>
       </ul>
     </div>
+    <NavLink className="navbar-brand mx-auto fw-bold" to="/">FORTUNE MART</NavLink>
   </div>
 </nav>
     </>
