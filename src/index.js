f5160786b09c2a5607ad2687f44611170e6a90a7
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-03-21 11:56:08 +0100

    remove BrowserRouter

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom'
 import '../node_modules/bootstrap/dist/css/bootstrap-grid.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
-
 ReactDOM.render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
+    </React.StrictMode>,
   document.getElementById('root')
 );
 
