14c49649e356f5014d361bda801faf8dc014aaeb
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-03-22 13:42:04 +0100

    render footer

--- src/App.js
+++ src/App.js
@@ -13,14 +13,13 @@ function App() {
       <BrowserRouter>
       <Header />
         <Routes>
-          <Route path="/" element={Home }/>
-          <Route path="/about" element={About}/>
-          <Route path="/contact" element={Contact}/>
-          <Route path="/product" element={Product}/>
+          <Route path="/" element={<Home /> }/>
+          <Route  path="about"elementt={<About />}/>
+          <Route  path="contact" element={<Contact />}/>
+          <Route  path="product" element={<Product />}/>
         </Routes>
-        {/* <Footer /> */}
       </BrowserRouter>
-      <Home />
+        <Footer />
     </div>
   );
 }
