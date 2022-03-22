b3b9870aece46c8349250da420fc1d2c97d00d35
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-03-22 14:56:12 +0100

    fix about bug

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Header from './Components/Header'
 import Footer from './Components/Footer'
 import Home from './Components/Home'
-import About from './Components/About'
+import About from "./Components/About";
 import Contact from './Components/Contact'
 import Product from './Components/Product'
 import {BrowserRouter,Routes,Route,} from "react-router-dom";
@@ -14,7 +14,7 @@ function App() {
       <Header />
         <Routes>
           <Route path="/" element={<Home /> }/>
-          <Route  path="about"elementt={<About />}/>
+          <Route  path="about"element={<About />}/>
           <Route  path="contact" element={<Contact />}/>
           <Route  path="product" element={<Product />}/>
         </Routes>
