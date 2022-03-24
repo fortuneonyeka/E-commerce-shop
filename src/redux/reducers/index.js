2ba0802042e0fa6501a31c9b05db849d6c2598dc
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-03-24 13:15:00 +0100

    create rootReducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,9 @@
-// import addItems from "./addItem";
-// import { combineReducers } from "redux";
+import addItems from "./addItem";
+import { combineReducers } from "redux";
 
 
-// const rootReducers = combineReducers({
-//   addItems
-// })
+const rootReducers = combineReducers({
+  addItems
+})
 
-// export default rootReducers;
+export default rootReducers;
