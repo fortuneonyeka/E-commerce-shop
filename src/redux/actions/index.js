0f461123d48e711af2ca2f0dd56c91703815e65c
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-03-24 13:13:46 +0100

    create redux actions

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,14 +1,14 @@
-// export const addItem = (product) => {
-//   return {
-//     type: "ADDITEM",
-//     payload: product,
-//   }
-// }
+export const addItem = (product) => {
+  return {
+    type: "ADDITEM",
+    payload: product,
+  }
+}
 
 
-// export const delItem = (product) => {
-//   return {
-//     type : "DELITEM",
-//     payload: product
-//   }
-// }
+export const delItem = (product) => {
+  return {
+    type : "DELITEM",
+    payload: product
+  }
+}
