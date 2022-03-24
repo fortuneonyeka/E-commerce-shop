d1a401dccb7e73a8b32455b7a1afecaf52ecc71c
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-03-24 13:07:18 +0100

    implement cart counter and display length

--- src/Components/buttons/CartBtn.jsx
+++ src/Components/buttons/CartBtn.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { NavLink } from "react-router-dom"
+import { useSelector } from "react-redux"
+
 
 const CartBtn = () => {
+  const counter = useSelector((state)=> state.addItems)
   return (
     <>
     <NavLink to="/cart" className="btn btn-outline-primary ms-2">
-      <span className="fa fa-shopping-cart me-1"></span> Cart(0)
+      <span className="fa fa-shopping-cart me-1"></span> Cart({counter.length})
     </NavLink>
     </>
   )
