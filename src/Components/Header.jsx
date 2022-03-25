ec501679deda2900e60ca617d786be6e25c3a29a
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-03-25 16:06:42 +0100

    comment navbar fixed style

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <>
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
+  <div className="container-fluid okay">
     
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
